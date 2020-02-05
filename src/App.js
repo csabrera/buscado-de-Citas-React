fd2c19b28e3dacbd6f13eeac86bab9e1169f5b86
Carlos Sabrera <csabreracaballero@gmail.com>
2020-02-05 09:58:48 -0500

    Proyecto de Buscador de Citas con una API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+
+import Frase from './components/Frase'
+
+import styled from '@emotion/styled'
+
+const Contenedor = styled.div`
+  display : flex;
+  align-items : center;
+  padding-top : 10rem;
+  flex-direction : column;
+
+`
+
+const Button = styled.button`
+  background : -webkit-linear-gradient(top left, #007D35 0%, #007D35 40%, #0F574E 100%);
+  background-size : 300px;
+  font-family : Arial, Helvetica, sans-serif;
+  color : #FFF;
+  margin-top : 3rem;
+  padding : 1rem 3rem;
+  font-size : 2rem;
+  border : 2px solid black;
+  transition : background-size .8s ease;
+
+  :hover {
+    cursor : pointer;
+    background-size : 400;
+  }
+`;
 
 function App() {
+
+  //State de Frase
+  const [ frase, guardarFrase ] = useState({})
+
+  const consultarApi = async () => {
+    const api = await fetch('http://breaking-bad-quotes.herokuapp.com/v1/quotes');
+    const frase = await api.json();
+    guardarFrase(frase[0])
+  }
+
+  //Cargar una Frase
+  useEffect( () => {
+    consultarApi()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Contenedor>
+      <Frase
+        frase = { frase }
+      >
+      </Frase>
+      <Button
+        onClick = { consultarApi }
+      >
+        Obtener frase
+      </Button>
+    </Contenedor>
+    
   );
 }
 
